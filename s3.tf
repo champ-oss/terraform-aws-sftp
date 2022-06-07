1cb91d35f72b032ca342cc50bd6b151a81b7e2db
Justin Reeves <reeves122@gmail.com>
2022-06-07 12:53:24 -0500

    add sync workflow. use local.name

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ module "s3" {
   count           = var.bucket == "" ? 1 : 0 # Create a bucket if one is not passed in
   source          = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.3-0cbbc69"
   git             = var.git
-  name            = var.name
+  name            = local.name
   use_name_prefix = false
   protect         = var.protect
   versioning      = true
