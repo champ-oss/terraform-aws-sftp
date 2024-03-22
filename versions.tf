ff585e083017e54cd5e222807e5b906ade7b2688
Justin Reeves <reeves122@gmail.com>
2024-03-22 11:15:05 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.71.0"
+      version = ">= 5.40.0"
     }
   }
 }
